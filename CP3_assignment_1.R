86146636aaa526132bb9d4cb1b3b1d417e7b7d35
aidanohara1 <aohara1@bu.edu>
2023-08-16 12:58:58 -0400

    ready

---
+++ CP3_assignment_1.R
@@ -0,0 +1,29 @@
+# YOUR NAME HERE #
+
+# Writing Functions 1
+
+# Factorial, Fibonacci, and Foobar examples
+
+# Exercise 1
+
+# Factorial
+
+myFactorial <- function(int) {
+ 
+}
+
+# Exercise 2
+
+# Fibonacci
+
+myFib <- function(n) {
+  
+}
+
+# Exercise 3
+
+# FooBar
+
+fooBarFun <- function(i) {
+  
+}
